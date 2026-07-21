a7660459af3f665e409c75370a94d7a15b07e19e
JDraszcz <j.draszcz@outlook.fr>
2026-07-21 22:37:44 +0200

    End of the starter version of the madlibs game

--- First Exercises/madlibs.py
+++ First Exercises/madlibs.py
@@ -12,3 +12,30 @@
 # First I will write a story one
 # Then ask the user a word for each blank
 # Then write it again with the words the user used
+
+story = f"Once upon a ____, living in the forest of ________ I was _______ by the Chancelor"
+
+print("---------------------------------------------------------------")
+print("         Fill the blanks in the following sentence             ")
+print("---------------------------------------------------------------")
+print(" ")
+print(" ")
+print(" ")
+print(story)
+print(" ")
+print(" ")
+print(" ")
+choice_1 = input("Choose the first word    ")
+print(" ")
+print(" ")
+print(" ")
+choice_2 = input("Choose the second word   ")
+print(" ")
+print(" ")
+print(" ")
+choice_3 = input("Choose the third word    ")
+print(" ")
+print(" ")
+print(" ")
+story = f"Once upon a {choice_1}, living in the forest of {choice_2} I was {choice_3} by the Chancelor"
+print(story)
