6558cede36fff1477f8f10d77567e00b2fd0d92c
JDraszcz <j.draszcz@outlook.fr>
2026-09-07 10:18:33 +0200

    CSV Reader problem solved

--- Advanced Exercises/Reading Files/main.py
+++ Advanced Exercises/Reading Files/main.py
@@ -1,7 +1,7 @@
 # Let's now read the files we have created previously
 
 # First we have to get their path 
-# file_path = "C:/Users/jordan.draszcz/OneDrive - SPIE/Documents/Cours Ecole/Python_Course/Advanced Exercises/Reading Files/list.csv"
+file_path = "C:/Users/jordan.draszcz/OneDrive - SPIE/Documents/Cours Ecole/Python_Course/Advanced Exercises/Reading Files/list.csv"
 
 # Then we will use the with statement, and since we did use the 
 # open() method previously, we will change the letter to r for "read"
@@ -23,20 +23,16 @@
 
 # Last we have to read the csv file, first we import the csv module
 
-# import csv
+import csv
 
-# with open(file_path, "r") as file:
+with open(file_path, "r") as file:
     # First let's have access to the file
-#     content = csv.reader(file)
+    content = csv.reader(file)
     # However to write it we did had to write it row by row
     # The concept is the same in order to read it
-#     for line in content:
-#         if line : 
+    for line in content:
             # This one print row by row
-#             print(line[0])
+            print(line[0])
         # if you want to print everything
-#         print(line)
+    print(line)
 
-
-# There's an error which indicates that's I'm out of range which starts at 0 
-# illogical
