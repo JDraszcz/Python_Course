2fed37399200c454a138921c4ccd95a7c1f58e6a
JDraszcz <j.draszcz@outlook.fr>
2026-09-07 13:24:11 +0200

    PIP don't want to be installed, can't do the alarm Clocl

---
+++ Advanced Exercises/alarm clock.py
@@ -0,0 +1,19 @@
+# In this project we are going to create a fully fonctionnal alarm clock
+# A sound might be needed
+# We are going to use the time module
+# I'm not going to code my idea but here it is
+# We will import the date and timedate module
+# We will set the targeted date using the timedate module
+# We will use the sleep method of the time module in a 
+# while loop
+# And finally we ring the bell 
+
+# Here's the code from the video
+import time
+import datetime
+import pygame
+# The pygame library is used to play a sound
+
+
+# THIS PROGRAM CAN'T BE MADE SINCE THE PYGAME LIBRARY CAN'T BE INSTALLED
+
